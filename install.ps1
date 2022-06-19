617ceaab40f55fb03e5ab2594524d5f77314166e
Jan Egil Ring <jan.egil.ring@crayon.com>
2022-06-19 08:11:08 +0200

    Updated logic for .config symlinks
    Signed-off-by: Jan Egil Ring <jan.egil.ring@crayon.com>

--- install.ps1
+++ install.ps1
@@ -1,9 +1,25 @@
-$configPath = (Join-Path $PSScriptRoot '.config')
+$configPath = "~/.config"
+
+if (Test-Path $configPath) {
+    "Config path $configPath exists" >> ~\dotfiles.log
+
+} else {
+    "Config path $configPath does not exist - creating" >> ~\dotfiles.log
+
+    New-Item -Path ~/.config -ItemType Directory
+
+}
 
 # Set up symlinks
-Get-ChildItem -Path $configPath | ForEach-Object {
+$customConfigPath = (Join-Path $PSScriptRoot '.config')
+Get-ChildItem -Path $customConfigPath | ForEach-Object {
     New-Item -Path (Join-Path '~/.config' $_.Name) -ItemType SymbolicLink -Value $_.FullName
 }
 
+
 Install-Module PSDepend -Force
-Invoke-PSDepend -Force (Join-Path $PSScriptRoot 'requirements.psd1')
+
+#Disabled for testing
+#Invoke-PSDepend -Force (Join-Path $PSScriptRoot 'requirements.psd1')
+
+"install.ps1 completed $(Get-Date)" >> ~\dotfiles.log
