7cd57384784c69c44a9d0f2eceac35006304e480
Jan Egil Ring <jan.egil.ring@outlook.com>
2023-08-06 15:45:49 +0200

    Updated to synced PowerShell profile
    Signed-off-by: Jan Egil Ring <jan.egil.ring@outlook.com>

--- .config/powershell/profile.ps1
+++ .config/powershell/profile.ps1
@@ -1,77 +1,153 @@
-$AvailableModules = Get-Module -ListAvailable | Select-Object -ExpandProperty name
-$WorkspaceFolder = Join-Path -Path (Get-ChildItem /workspaces | Select-Object -First 1).FullName -ChildPath .devcontainer/
-$WorkspacePowerShellFolder = Join-Path -Path $WorkspaceFolder -ChildPath powershell
+#Requires -Version 7
 
-if (-not (Test-Path -Path $WorkspacePowerShellFolder)) {
-    $null = New-Item -Path $WorkspacePowerShellFolder -ItemType Directory -Force
-}
+# Version 1.3.6
 
-if ($AvailableModules -contains 'Terminal-Icons') {
-    Import-Module -Name Terminal-Icons
-}
+# Cross-platform PowerShell profile based on https://devblogs.microsoft.com/powershell/optimizing-your-profile/
 
-# PSReadLine
+# check if newer version
+$gistUrl = 'https://api.github.com/gists/a4e366e6b01f0b70eae90557dfc9d21d'
+$latestVersionFile = [System.IO.Path]::Combine("$HOME", '.latest_profile_version')
+$versionRegEx = '# Version (?<version>\d+\.\d+\.\d+)'
 
-Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
-
-if ($IsLinux) {
-
-    if ($AvailableModules -contains 'Microsoft.PowerShell.UnixCompleters') {
-        Import-Module Microsoft.PowerShell.UnixCompleters
-        Import-UnixCompleters -ErrorAction Ignore
+if ([System.IO.File]::Exists($latestVersionFile)) {
+    $latestVersion = [System.IO.File]::ReadAllText($latestVersionFile)
+    $currentProfile = [System.IO.File]::ReadAllText($profile.CurrentUserAllHosts)
+    [version]$currentVersion = '0.0.0'
+    if ($currentProfile -match $versionRegEx) {
+        $currentVersion = $matches.Version
     }
 
-    Set-PSReadLineOption -HistorySavePath "$WorkspacePowerShellFolder/PSReadLine_history.txt"
-
-}
-
-Set-PSReadLineOption -MaximumHistoryCount 32767
-
-switch ($PSVersionTable.PSVersion.Major) {
-    7 {
-        #Set-PSReadLineOption -PredictionSource HistoryAndPlugin #7.2 or a higher version of PowerShell is required
-        Set-PSReadLineOption -PredictionSource History
-        Set-PSReadLineOption -PredictionViewStyle ListView -WarningAction SilentlyContinue
-    }
-    default {
-        Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward # Must be disabled for Az.Tools.Predictor/ListView to work
-        Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
-        Set-PSReadLineOption -PredictionViewStyle InlineView
-        Set-PSReadLineOption -PredictionSource History
+    if ([version]$latestVersion -gt $currentVersion) {
+        Write-Verbose "Your version: $currentVersion" -Verbose
+        Write-Verbose "New version: $latestVersion" -Verbose
+        $choice = Read-Host -Prompt 'Found newer profile, install? (Y)'
+        if ($choice -eq 'Y' -or $choice -eq '') {
+            try {
+                $gist = Invoke-RestMethod $gistUrl -ErrorAction Stop
+                $gistProfile = $gist.Files.'profile.ps1'.Content
+                Set-Content -Path $profile.CurrentUserAllHosts -Value $gistProfile
+                Write-Verbose 'Installed newer version of profile' -Verbose
+                . $profile
+                return
+            } catch {
+                # we can hit rate limit issue with GitHub since we're using anonymous
+                Write-Verbose -Verbose 'Was not able to access gist, try again next time'
+            }
+        }
     }
 }
 
-function Set-EnvVar {
-    if (Test-Path -Path ~/.Azure/AzureRmContext.json) {
-        $azureContext = Get-Content ~/.Azure/AzureRmContext.json | ConvertFrom-Json
-        $subscriptionName = $azureContext.Contexts.($azureContext.DefaultContextKey).Subscription.Name
-        $env:oh_my_azure_context = $subscriptionName
-    } else {
-        $env:oh_my_azure_context = $null
+$global:profile_initialized = $false
+
+
+# Useful before doing demos, where default settings might be preferred
+function Reset-Console {
+
+    function global:prompt { "PS $($executionContext.SessionState.Path.CurrentLocation)$('>' * ($nestedPromptLevel + 1)) " }
+
+    Set-Location ~
+
+}
+
+function prompt {
+
+    function Initialize-Profile {
+
+        $null = Start-ThreadJob -Name "Get version of `$profile from gist" -ArgumentList $gistUrl, $latestVersionFile, $versionRegEx -ScriptBlock {
+            param ($gistUrl, $latestVersionFile, $versionRegEx)
+
+            try {
+                $gist = Invoke-RestMethod $gistUrl -ErrorAction Stop
+
+                $gistProfile = $gist.Files.'profile.ps1'.Content
+                [version]$gistVersion = '0.0.0'
+                if ($gistProfile -match $versionRegEx) {
+                    $gistVersion = $matches.Version
+                    Set-Content -Path $latestVersionFile -Value $gistVersion
+                }
+            } catch {
+                # we can hit rate limit issue with GitHub since we're using anonymous
+                Write-Verbose -Verbose 'Was not able to access gist to check for newer version'
+            }
+        }
+
+        if ((Get-Module PSReadLine).Version -lt 2.2) {
+            throw 'Profile requires PSReadLine 2.2+'
+        }
+
+        #Enable concise errorview for PS7 and later
+        if ($PSVersionTable.PSVersion.Major -ge 7) {
+            $ErrorView = 'ConciseView'
+        }
+
+        # setup psdrives
+
+        if (!(Test-Path repos:)) {
+            if (Test-Path ([System.IO.Path]::Combine("$HOME", 'repos'))) {
+                New-PSDrive -Root ~/repos -Name repos -PSProvider FileSystem -Scope Global > $Null
+                Set-Location repos:
+            }
+        }
+
+        Set-PSReadLineOption -Colors @{ Selection = "`e[92;7m"; InLinePrediction = "`e[36;7;238m" } -PredictionSource History
+        Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+        Set-PSReadLineKeyHandler -Chord Ctrl+b -Function BackwardWord
+        Set-PSReadLineKeyHandler -Chord Ctrl+f -Function ForwardWord
+        Set-PSReadLineOption -MaximumHistoryCount 32767
+        Set-PSReadLineOption -PredictionViewStyle ListView
+
+        if ($IsWindows) {
+            Set-PSReadLineOption -EditMode Emacs -ShowToolTips
+            Set-PSReadLineKeyHandler -Chord Ctrl+Shift+c -Function Copy
+            Set-PSReadLineKeyHandler -Chord Ctrl+Shift+v -Function Paste
+        } else {
+            try {
+                Import-UnixCompleters
+            } catch [System.Management.Automation.CommandNotFoundException] {
+                Install-Module Microsoft.PowerShell.UnixCompleters -Repository PSGallery -AcceptLicense -Force
+                Import-UnixCompleters
+            }
+        }
+
+        if (Get-Command -Name oh-my-posh -ErrorAction Ignore) {
+
+            if ($IsMacOS) {
+                oh-my-posh init pwsh --config "$(brew --prefix oh-my-posh)/themes/night-owl.omp.json" | Invoke-Expression
+            } else {
+                oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\night-owl.omp.json" | Invoke-Expression
+            }
+
+        }
+
     }
 
-    $env:oh_my_psversion = ($PSVersionTable.PSVersion.ToString() -split '-')[0]
-}
-New-Alias -Name 'Set-PoshContext' -Value 'Set-EnvVar' -Scope Global
 
-if (Get-Command -Name 'oh-my-posh' -ErrorAction SilentlyContinue) {
+    if ($global:profile_initialized -ne $true) {
+        $global:profile_initialized = $true
+        Initialize-Profile
+        if (Get-PSDrive -Name repos -ErrorAction Ignore) {
+            Set-Location repos:
+        }
+    }
 
-   oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/night-owl.omp.json' | Invoke-Expression
+    $currentLastExitCode = $LASTEXITCODE
+    $lastSuccess = $?
 
-   # if (Test-Path -Path "$WorkspacePowerShellFolder/themes/jan.json") {
+    # set window title
+    try {
+        $prefix = ''
+        if ($isWindows) {
+            $identity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+            $windowsPrincipal = [Security.Principal.WindowsPrincipal]::new($identity)
+            if ($windowsPrincipal.IsInRole('Administrators') -eq 1) {
+                $prefix = 'Admin:'
+            }
+        }
 
-   #     oh-my-posh init pwsh --config "$WorkspacePowerShellFolder/themes/jan.json" | Invoke-Expression
+        $Host.ui.RawUI.WindowTitle = "$prefix$PWD"
+    } catch {
+        # do nothing if can't be set
+    }
 
-   # } else {
-
-   #     oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/jandedobbeleer.omp.json'
-
-   # }
-}
-
-New-Alias -Name k -Value kubectl -Scope Global
-
-#Enable concise errorview for PS7 and up
-if ($psversiontable.psversion.major -ge 7) {
-    $ErrorView = 'ConciseView'
+    $global:LASTEXITCODE = $currentLastExitCode
 }
